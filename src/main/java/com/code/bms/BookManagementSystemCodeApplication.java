4d268141f7af59d8fd9f965e2479ec0d54cda922
seungwon <swlee173@gmail.com>
2025-02-22 00:20:25 +0900

    init

---
+++ src/main/java/com/code/bms/BookManagementSystemCodeApplication.java
@@ -0,0 +1,13 @@
+package com.code.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementSystemCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookManagementSystemCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/code/bms/BookManagementSystemCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementSystemCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
