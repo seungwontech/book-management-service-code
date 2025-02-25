8e03ab53248a747347a03ed28266ff52b03ba568
seungwon <swlee173@gmail.com>
2025-02-25 17:22:22 +0900

    rename: BookServiceUnitTest > BookServiceTest로 파일명 변경

--- src/test/java/com/code/bms/unit/BookServiceTest.java
+++ src/test/java/com/code/bms/unit/BookServiceTest.java
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class BookServiceUnitTest {
+class BookServiceTest {
 
     @Mock
     private BookRepository bookRepository;
